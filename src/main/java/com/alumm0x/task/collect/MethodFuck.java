1035d1fe7fcbd93882b371cc6c136b319aed8353
alumm0x <zhoufeng9186@cvte.com>
2024-03-13 09:30:35 +0800

    优化MethodFuck，构造无效参数尝试，请求失败则存在问题

--- src/main/java/com/alumm0x/task/collect/MethodFuck.java
+++ src/main/java/com/alumm0x/task/collect/MethodFuck.java
@@ -13,8 +13,12 @@ import com.alumm0x.util.BurpReqRespTools;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class MethodFuck extends VulTaskImpl {
+
+    // 标记此次检测的编号
+    String uuid = UUID.randomUUID().toString().substring(0, 8);
 
     public static VulTaskImpl getInstance(HttpRequestResponseWithMarkers requestResponse){
         return new MethodFuck(requestResponse);
@@ -42,13 +46,18 @@ public class MethodFuck extends VulTaskImpl {
             if (methods.contains(c_method)) {
                 methods.remove(c_method); //删除已有的，检测其他的
                 for (String method : methods) {
+                    String reqBody = new String(BurpReqRespTools.getReqBody(requestResponse));
+                    // 如果原始是get的，探测其他类型的请求则默认填充请求体{}
+                    if ("".equalsIgnoreCase(reqBody) && !method.equalsIgnoreCase("get")) {
+                        reqBody = "{}";
+                    }
                     //新的请求包
                     okHttpRequester.send(
                         BurpReqRespTools.getUrlWithOutQuery(requestResponse), 
                         method, 
                         BurpReqRespTools.getReqHeaders(requestResponse), 
                         BurpReqRespTools.getQuery(requestResponse), 
-                        new String(BurpReqRespTools.getReqBody(requestResponse)), 
+                        reqBody, 
                         BurpReqRespTools.getContentType(requestResponse), 
                         new MethodFuckCallback(this));
                 }
@@ -83,9 +92,10 @@ class MethodFuckCallback implements Callback {
     public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
         String message = null;
         HttpRequestResponseWithMarkers requestResponse = new HttpRequestResponseWithMarkers(BurpReqRespTools.makeBurpReqRespFormOkhttp(call, response, vulTask.requestResponse));
-        if (BurpReqRespTools.getStatus(requestResponse) == 200
-                || BurpReqRespTools.getStatus(requestResponse) == 302) {
-            message = "发现同一接口存在多请求方式，OriginMethod=" + BurpReqRespTools.getMethod(vulTask.requestResponse);
+        // 常规同一个请求支持多种请求方式的话，一定会存在请求参数的差异
+        // 请求参数不符合要求是会请求失败的
+        if (!response.isSuccessful()) {
+            message = String.join("【%s】发现同一接口存在多请求方式，OriginMethod=%s", ((MethodFuck)vulTask).uuid, BurpReqRespTools.getMethod(vulTask.requestResponse));
         }
         // 记录日志
         MainPanel.logAdd(
